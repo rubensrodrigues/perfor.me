a675f7ccd6a001b6ad5dfa6c0d2a0458962f327f
Rubens M. S. Rodrigues <rubens.rodrigues@duxcod3.com>
2024-03-04 17:36:03 -0300

    ajustes provider do JWTHelper

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,30 @@
-import { ApplicationConfig } from '@angular/core';
+import { JwtModule, JwtModuleOptions } from '@auth0/angular-jwt';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { AppConstants } from './comum/app-constants';
+import { LoginComponent } from './components/login/login.component';
+import { HttpClient, HttpHandler, provideHttpClient } from '@angular/common/http';
+
+export function tokenGetter() {
+  return localStorage.getItem(AppConstants.tokenName);
+}
+const options : JwtModuleOptions = {
+  config: {
+    tokenGetter: tokenGetter,
+    allowedDomains: ["localhost"],
+    disallowedRoutes: [""],
+  },
+}
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideRouter(routes),
+    provideClientHydration(),
+    importProvidersFrom(JwtModule.forRoot(options)),
+    LoginComponent,
+    provideHttpClient()
+  ]
 };
